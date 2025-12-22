6519350274f54396ce83e0c50f845360b8d77752
Gopinath <gopinathbalu07@gmail.com>
2025-12-22 11:57:36 +0530

    Changes Done

--- shopownerservice/src/main/java/com/tnsif/shopownerservice/config/ShopOwnerConfig.java
+++ shopownerservice/src/main/java/com/tnsif/shopownerservice/config/ShopOwnerConfig.java
@@ -2,29 +2,22 @@ package com.tnsif.shopownerservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 @Configuration
-@CrossOrigin(origins = "http://localhost:3000")  // React connection
 public class ShopOwnerConfig {
-	
-	@Bean
+
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins("https://shoppers-front.netlify.app")
+                        .allowedMethods("*")
                         .allowedHeaders("*");
             }
         };
-	}
-	
+    }
 }
-      
-
-
